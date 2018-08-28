e4a4f368fa15a1a648725b37ad9cc5dce5a21728
mahesh.siddalingappa <mahesh.siddalingappa@samba.tv>
2018-08-27 19:00:08 -0700

    added post clone script for appcenter

---
+++ appcenter-post-clone.sh
@@ -0,0 +1,29 @@
+echo "Installing NVM..."
+brew install nvm
+source $(brew --prefix nvm)/nvm.sh
+
+echo "Installing v8.5..."
+nvm install v8.5.0
+nvm use --delete-prefix v8.5.0
+nvm alias default v8.5.0
+
+echo "Detecting applesimutils"
+which applesimutils
+
+echo "Figuring out node version..."
+node --version
+
+echo "Installing detox cli..."
+npm install -g detox-cli
+
+echo "Installing dependencies for detox tests..."
+npm install
+
+echo "Building the project..."
+detox build 
+
+echo "Starting the packager"
+npm run start &
+
+echo "start the tests"
+npm run functional 
