ffe221d8a8a2bd8037a7d85a3c77e7c2c1be8d8e
mahesh.siddalingappa <mahesh.siddalingappa@samba.tv>
2018-08-27 21:17:01 -0700

    android specific post-clone script

--- appcenter-post-clone.sh
+++ appcenter-post-clone.sh
@@ -1,12 +1,3 @@
-echo "Installing applesimutils"
-mkdir simutils
-cd simutils
-curl https://raw.githubusercontent.com/wix/homebrew-brew/master/AppleSimulatorUtils-0.5.22.tar.gz -o applesimutils.tar.gz
-tar xzvf applesimutils.tar.gz
-sh buildForBrew.sh 
-cd ..
-export PATH=$PATH:./simutils/build/Build/Products/Release
-
 echo "Installing NVM..."
 brew install nvm
 source $(brew --prefix nvm)/nvm.sh
@@ -15,9 +6,6 @@ echo "Installing v8.5..."
 nvm install v8.5.0
 nvm use --delete-prefix v8.5.0
 nvm alias default v8.5.0
-
-echo "Detecting applesimutils"
-which applesimutils
 
 echo "Figuring out node version..."
 node --version
@@ -29,10 +17,10 @@ echo "Installing dependencies for detox tests..."
 npm install
 
 echo "Building the project..."
-xcodebuild -project ios/ndash.xcodeproj -scheme ndash -configuration Debug -sdk iphonesimulator -derivedDataPath ios/build
+cd android && ./gradlew assembleDebug assembleAndroidTest -DtestBuildType=debug && cd ..
 
 echo "Starting the packager"
 npm run start &
 
 echo "start the tests"
-npm run iosDetox 
+npm run androidDetox 
